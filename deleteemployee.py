6a69a52fd586207b705e233b7e132691c28b2e85
patidar007a <162842640+patidar007a@users.noreply.github.com>
2024-04-07 21:25:52 -0400

    Update deleteemployee.py

--- deleteemployee.py
+++ deleteemployee.py
@@ -1,16 +1,41 @@
+class Employee:
+    def __init__(self, id, first_name, last_name, dob, department, salary):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.dob = dob
+        self.department = department
+        self.salary = salary
+
 def delete_employee(employees):
-   
-
-    employee_id = input("Enter the ID of the employee to delete: ")
-    
+    employee_id = input("Enter the ID which you want to delete: ")
+    found = False
     for employee in employees:
-        if employee.get('id') == employee_id:
+        if employee.id == int(employee_id): 
             employees.remove(employee)
-            print("Employee deleted successfully!")
-            return
-    
-   
-    print("Employee with ID '{}' not found.".format(employee_id))
+            found = True
+            print("You successfully delete the employee.")
+            break
+    if not found:
+        print("Given employee ID is not found.")
 
 
-delete_employee(employees)
+    with open("employee_data.txt", "w") as file:
+        for employee in employees:
+            file.write(f"{employee.id},{employee.first_name},{employee.last_name},{employee.dob},{employee.department},{employee.salary}\n")
+
+
+if __name__ == "__main__":
+    employees = [] 
+
+    while True:
+        print("\n1. Delete Employee")
+        print("2. Exit")
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            delete_employee(employees)
+        elif choice == "2":
+            break
+        else:
+            print("Invalid choice. Please enter a valid option.")
